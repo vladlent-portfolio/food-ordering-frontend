067b56e1f912daf3ecaed39ea719323f7cbfd735
Vladlen Tereshchenko <vladlent.dev@gmail.com>
2021-05-05 16:12:24 +0300

    (admin): adds links rendering test to nav component

--- src/app/admin/components/nav/nav.component.spec.ts
+++ src/app/admin/components/nav/nav.component.spec.ts
@@ -1,13 +1,18 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing"
 
 import { AdminNavComponent } from "./nav.component"
+import { pages } from "../../admin-routing.module"
+import { MatListModule } from "@angular/material/list"
+import { RouterTestingModule } from "@angular/router/testing"
 
 describe("NavComponent", () => {
   let component: AdminNavComponent
   let fixture: ComponentFixture<AdminNavComponent>
+  let nativeEl: HTMLElement
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [RouterTestingModule, MatListModule],
       declarations: [AdminNavComponent],
     }).compileComponents()
   })
@@ -15,10 +20,22 @@ describe("NavComponent", () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(AdminNavComponent)
     component = fixture.componentInstance
+    nativeEl = fixture.nativeElement
     fixture.detectChanges()
   })
 
-  it("should create", () => {
-    expect(component).toBeTruthy()
+  it("should render navigation links from routing pages", async () => {
+    fixture.detectChanges()
+    const links = Array.from(nativeEl.querySelectorAll("a"))
+    expect(links).toHaveSize(pages.length)
+
+    links.forEach(a => {
+      const page = pages.find(page => a.href.includes(page.path as string))
+      expect(page).toBeDefined(`expected ${a.href} to contain some actual path`)
+
+      if (page) {
+        expect(a.textContent).toContain(page.data.title)
+      }
+    })
   })
 })
