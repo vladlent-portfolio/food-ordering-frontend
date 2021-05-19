9179dafc5d04480a9062843dcfa4b024554ceb9e
Vladlen Tereshchenko <vladlent.dev@gmail.com>
2021-05-19 21:25:12 +0300

    (app): adds basic confirm dialog component with tests

---
+++ src/app/components/dialogs/confirm/confirm.component.spec.ts
@@ -0,0 +1,102 @@
+import { ComponentFixture, TestBed } from "@angular/core/testing"
+import { ConfirmDialogComponent, ConfirmDialogData } from "./confirm.component"
+import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from "@angular/material/dialog"
+import { NoopAnimationsModule } from "@angular/platform-browser/animations"
+
+describe("ConfirmComponent", () => {
+  let component: ConfirmDialogComponent
+  let fixture: ComponentFixture<ConfirmDialogComponent>
+  let nativeEl: HTMLElement
+  let dialogRef: jasmine.SpyObj<MatDialogRef<ConfirmDialogComponent>>
+  let initData: ConfirmDialogData
+
+  beforeEach(() => {
+    initData = {}
+    dialogRef = jasmine.createSpyObj("MatDialogRef", ["close"])
+
+    TestBed.configureTestingModule({
+      declarations: [ConfirmDialogComponent],
+      imports: [MatDialogModule, NoopAnimationsModule],
+      providers: [
+        { provide: MAT_DIALOG_DATA, useValue: initData },
+        { provide: MatDialogRef, useValue: dialogRef },
+      ],
+    })
+  })
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ConfirmDialogComponent)
+    component = fixture.componentInstance
+    nativeEl = fixture.nativeElement
+  })
+
+  it("should have default values", () => {
+    Object.entries(component.data).forEach(([key, value]) => {
+      expect(value).toBeDefined(`expected ${key} to be defined`)
+    })
+  })
+
+  it("should toggle title", () => {
+    fixture.detectChanges()
+    expect(queryTitle().textContent).toContain(component.data.title)
+
+    component.data.title = null
+    fixture.detectChanges()
+    expect(queryTitle()).toBeNull()
+  })
+
+  it("should toggle content", () => {
+    fixture.detectChanges()
+    expect(queryContent().textContent).toContain(component.data.content)
+
+    component.data.content = null
+    fixture.detectChanges()
+    expect(queryContent()).toBeNull()
+  })
+
+  it("should merge options on init", () => {
+    Object.assign(initData, {
+      title: "New title",
+      content: "Important content",
+      confirmBtnText: "YES!",
+      cancelBtnText: "no",
+    })
+
+    component.ngOnInit()
+    expect(component.data).toEqual(initData as any)
+  })
+
+  describe("Confirm button", () => {
+    it("should close dialog with 'true'", () => {
+      fixture.detectChanges()
+      queryConfirmBtn().click()
+      expect(dialogRef.close).toHaveBeenCalledWith(true)
+    })
+  })
+
+  describe("Cancel button", () => {
+    it("should close dialog with 'false'", () => {
+      fixture.detectChanges()
+      queryCancelBtn().click()
+      expect(dialogRef.close).toHaveBeenCalledWith(false)
+    })
+  })
+
+  function queryTitle() {
+    return nativeEl.querySelector("[data-test='confirm-dialog-title']") as HTMLElement
+  }
+
+  function queryContent() {
+    return nativeEl.querySelector("[data-test='confirm-dialog-content']") as HTMLElement
+  }
+
+  function queryCancelBtn() {
+    return nativeEl.querySelector(
+      "[data-test='confirm-dialog-cancel']",
+    ) as HTMLButtonElement
+  }
+
+  function queryConfirmBtn() {
+    return nativeEl.querySelector("[data-test='confirm-dialog-ok']") as HTMLButtonElement
+  }
+})
---
+++ src/app/components/dialogs/confirm/confirm.component.ts
@@ -0,0 +1,38 @@
+import { Component, Inject, OnInit } from "@angular/core"
+import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material/dialog"
+
+type DialogData = {
+  title: string | null
+  content: string | null
+  confirmBtnText: string
+  cancelBtnText: string
+}
+
+export type ConfirmDialogData = Partial<DialogData>
+
+@Component({
+  selector: "app-confirm-dialog",
+  templateUrl: "./confirm.component.html",
+  styleUrls: ["./confirm.component.scss"],
+})
+export class ConfirmDialogComponent implements OnInit {
+  data: DialogData = {
+    title: "Confirm action",
+    content: "Are you sure?",
+    confirmBtnText: "OK",
+    cancelBtnText: "Cancel",
+  }
+
+  constructor(
+    @Inject(MAT_DIALOG_DATA) private initData: ConfirmDialogData,
+    private dialogRef: MatDialogRef<ConfirmDialogComponent>,
+  ) {}
+
+  ngOnInit(): void {
+    Object.assign(this.data, this.initData)
+  }
+
+  close(confirmed: boolean) {
+    this.dialogRef.close(confirmed)
+  }
+}
